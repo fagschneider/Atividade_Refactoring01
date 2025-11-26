733a77c33043da8898c157155087bf5d99d1071a
fagschneider <fagschneider@minha.fag.edu.br>
2025-11-25 21:57:31 -0300

    commit10
    commit10

--- src/classes/Rental.java
+++ src/classes/Rental.java
@@ -1,31 +1,30 @@
 package classes;
 
 public class Rental {
+    private Movie _movie;
+    private int _daysRented;
 
-	   private Movie _movie;
-	   private int _daysRented;
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
 
-	   public Rental(Movie movie, int daysRented) {
-	      _movie = movie;
-	      _daysRented = daysRented;
-	   }
+    public int getDaysRented() {
+        return _daysRented;
+    }
 
-	   public int getDaysRented() {
-	      return _daysRented;
-	   }
+    public Movie getMovie() {
+        return _movie;
+    }
 
-	   public Movie getMovie() {
-	      return _movie;
-	   }
+    
+    public double getCharge() {
+        return _movie.getCharge(_daysRented);
+    }
 
-	   public double getCharge() {
-	      return _movie.getCharge(_daysRented);
-	   }
+    
+    public int getFrequentRenterPoints() {
+        return _movie.getFrequentRenterPoints(_daysRented);
+    }
+}
 
-	   public int getFrequentRenterPoints() {
-	      if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
-	         return 2;
-	      }
-	      return 1;
-	   }
-	}
