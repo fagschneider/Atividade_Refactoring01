df118aedd5795426be08dd0aecab36ace46c6127
fagschneider <fagschneider@minha.fag.edu.br>
2025-11-25 22:10:22 -0300

    commit13
    commit13

--- src/classes/NewReleasePrice.java
+++ src/classes/NewReleasePrice.java
@@ -1,8 +1,11 @@
 package classes;
 
 public class NewReleasePrice extends Price {
-    @Override
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
+    }
+
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
     }
 }
--- src/classes/Price.java
+++ src/classes/Price.java
@@ -2,23 +2,5 @@ package classes;
 
 public abstract class Price {
     public abstract int getPriceCode();
-
-    public double getCharge(int daysRented) {
-        if (getPriceCode() == Movie.REGULAR) {
-            double result = 2;
-            if (daysRented > 2)
-                result += (daysRented - 2) * 1.5;
-            return result;
-        }
-        if (getPriceCode() == Movie.NEW_RELEASE) {
-            return daysRented * 3;
-        }
-        if (getPriceCode() == Movie.CHILDRENS) {
-            double result = 1.5;
-            if (daysRented > 3)
-                result += (daysRented - 3) * 1.5;
-            return result;
-        }
-        return 0;
-    }
+    public abstract double getCharge(int daysRented);
 }
